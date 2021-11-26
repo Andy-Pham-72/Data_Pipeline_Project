91d51d7319232b475dac03862684fc862ead3d76
Andy Pham <aqpham02@gmail.com>
2021-11-25 19:08:54 -0500

    major update

--- ticket_sale_data_pipeline.py
+++ ticket_sale_data_pipeline.py
@@ -1,140 +1,135 @@
-#from configparser import Error
 import pandas as pd
 import mysql.connector
 import json
-
-import logging
-from logging.handlers import RotatingFileHandler
-
-def setup_logger():
-    """function creates log files"""
-    MAX_BYTES = 10000000 # Maximum size for a log file
-
-    # The name should be unique, so you can get in in other places
-    logger = logging.getLogger(__name__) 
-    logger.setLevel(logging.INFO) # the level should be the lowest level set in handlers
-    
-    # log format
-    log_format = logging.Formatter('[%(levelname)s] %(asctime)s - %(message)s')
-
-    # info logger
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(log_format)
-    stream_handler.setLevel(logging.INFO)
-    logger.addHandler(stream_handler)
-
-    info_handler = RotatingFileHandler('info.log', maxBytes=MAX_BYTES)
-    info_handler.setFormatter(log_format)
-    info_handler.setLevel(logging.INFO)
-    logger.addHandler(info_handler)
-
-    # error logger
-    error_handler = RotatingFileHandler('error.log', maxBytes=MAX_BYTES)
-    error_handler.setFormatter(log_format)
-    error_handler.setLevel(logging.ERROR)
-    logger.addHandler(error_handler)
-    return logger
-
-logger = setup_logger()
+# import module
+from setup_logger import logger
 
 # open config json file
-f = open(input("Type your config file directory:")) # data/config.json
+f = open("config/config.json") # data/config.json
 
 # load json object
 config = json.load(f)
 
-# input csv file directory
-file_path_csv= input("Type your csv file directory:")
+class SqlFuncs:
+    """
+    class SqlFuncs is created to access the essential mysql.connector's functions
+    """
 
-def get_db_connection():
-    """ function connects to MySQL server"""
-    global connection
-    connection = None
-    try:
-        connection = mysql.connector.connect(**config)
-        logger.info('Successfully connected to MySQL Server')
-    except Exception as err:
-        print("Error while connecting to database for job tracker\nPlease try again!\n", err)
-        # log
-        logger.error("Error while connecting to database for job tracker: {}".format(err))
-        # call again
-        get_db_connection()
+    def __init__(self):
+        self._conn = mysql.connector.connect(**config)  # use your password here
+        self._cursor = self._conn.cursor()
 
-    return connection
+    def __enter__(self):
+        return self
 
-def load_third_party(connection, file_path_csv):
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
 
-    cursor = connection.cursor()
-    # [Iterate through the CSV file and execute insert statement]
+    @property
+    def connection(self):
+        return self._conn
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    def execute(self, sql, params=None):
+        """Implements execute function"""
+        self.cursor.execute(sql, params or ())
+
+    def commit(self):
+        """Implements commit function"""
+        self.connection.commit()
+
+    def fetchall(self):
+        """Implements fetchall function"""
+        return self.cursor.fetchall()
+
+    def fetchone(self):
+        """Implement fetchone function"""
+        return self.cursor.fetchone()
+
+    def close(self, commit=True):
+        """Implement close function"""
+        if commit:
+            self.commit()
+        self.connection.close()
+
+class ticket_sale(SqlFuncs):
+
+    def __init__(self):
+        super().__init__()
     
-    try:
-        csv_file = pd.read_csv(file_path_csv, header= None, index_col = False, delimiter= ',')
-        for i, row in csv_file.iterrows():
-            my_sql_query = """INSERT INTO ticketsale (  ticket_id ,
-                                                        trans_date ,
-                                                        event_id ,
-                                                        event_name ,
-                                                        event_date ,
-                                                        event_type ,
-                                                        event_city ,
-                                                        customer_id,
-                                                        price,
-                                                        num_tickets )
-                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
-            cursor.execute(my_sql_query, tuple(row))
-            print("Record inserted")
-            connection.commit()
-        logger.info('Successfully insert data to ticketsale table')
-    except TypeError as err:
-        print("\n*************\nInvalid!\n*************\nPlease try again!\n")
-        # Log
-        logger.error("Invalid. An error message: {}".format(err))
-        
-    except ValueError as err:
-        print("\n*************\nInvalid input!\n*************\nPlease try again!\n")
-        # Log
-        logger.error("Invalid input. An error message: {}".format(err))
-        
-    except Exception as err:
-        print("Error while connecting to database for job tracker\nPlease try again!\n", err)
-        # log
-        logger.error("Error while connecting to database for job tracker: {}".format(err))
-        
-    return 
+    def load_third_party(self):
+        '''
+        function that loads data from ticket sale csv file and insert into the "ticketsale" database
+        '''
 
-def query_popular_tickets(connection):
-    """function runs the popular ticket query"""
-    
-    sql_statement = 'SELECT event_name \
-                    FROM ticketsales.ticketsale \
-                    ORDER BY num_tickets DESC ,trans_date ASC \
-                    LIMIT 3;'
-    try:
-        cursor = connection.cursor()
-        cursor.execute(sql_statement)
-        records = cursor.fetchall()
-        cursor.close()
-        if len(records) < 1:
-            logger.info('Successfully implement "the most popular tickets" query. But no updated output.')
-        else: 
-            print('Here are the most popular tickets in the past month:\n{}'.\
-                format('\n'.join(["- " + records[i][0] for i in range(0, len(records))])))
-            logger.info('Successfully implement "the most popular tickets" query')
-    except Exception as err:
-        print("Error while connecting to database for job tracker\nPlease try again!\n", err)
-        # log
-        logger.error("Error while connecting to database for job tracker: {}".format(err))
+        try:
+            # csv file directory
+            self.file_path_csv = "data/third_party_sales.csv"
+            csv_file = pd.read_csv(self.file_path_csv, header= None, index_col = False, delimiter= ',')
+            # Iterate through the CSV file and execute insert statement
+            for i, row in csv_file.iterrows():
+                my_sql_query = """ INSERT INTO ticketsale ( ticket_id ,
+                                                            trans_date ,
+                                                            event_id ,
+                                                            event_name ,
+                                                            event_date ,
+                                                            event_type ,
+                                                            event_city ,
+                                                            customer_id,
+                                                            price,
+                                                            num_tickets )
+                                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
+                self.execute(my_sql_query, tuple(row))
+                print(f"Record inserted #{i}")
+                self.commit()
+            logger.info('Successfully insert data to ticketsale table')
+        except AttributeError as err:
+            # Log
+            logger.error("Invalid. An error message: {}".format(err))
+        except TypeError as err:
+            # Log
+            logger.error("Invalid. An error message: {}".format(err))
+        except ValueError as err:
+            # Log
+            logger.error("Invalid input. An error message: {}".format(err))
+        except Exception as err:
+            # log
+            logger.error("Error while connecting to database for job tracker: {}".format(err))
+
+    def query_popular_tickets(self):
+        """Function runs the popular ticket query and give an output with the first 3 result as example below:
+            " Here are the most popular tickets in the past month:
+              - Washington Spirits vs Sky Blue FC
+              - The North American International Auto Show
+              - Christmas Spectacular 
+            "
+        """
+        # sql query
+        sql_statement = 'SELECT event_name \
+                        FROM ticketsales.ticketsale \
+                        ORDER BY num_tickets DESC ,trans_date ASC \
+                        LIMIT 3;'
+        try:
+            self.execute(sql_statement)
+            records = self.fetchall()
+            self.close()
+            if len(records) < 1:
+                logger.info('Successfully implemented "the most popular tickets" query. But no updated output.')
+            else: 
+                print('\nHere are the most popular tickets in the past month:\n{}'.\
+                    format('\n'.join(["- " + records[i][0] for i in range(0, len(records))])))
+                logger.info('Successfully implemented "the most popular tickets" query')
+        except Exception as err:
+            # log
+            logger.error("Error while connecting to database for job tracker: {}".format(err))
 
 if __name__ == '__main__':
 
-    connection = get_db_connection()
+    connection = ticket_sale()
 
-    try:
-        load_third_party(connection, file_path_csv)
-    except TypeError as err:
-        print("\n*************\nInvalid!\n*************\nPlease try again!\n")
-        # Log
-        logger.error("Invalid. An error message: {}".format(err))
+    connection.load_third_party()
 
-    query_popular_tickets(connection)
-
+    connection.query_popular_tickets()
